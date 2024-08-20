610f515b6ce7c9f7e184c48059bf39f24635110d
lce0517 <ashaeun2010@gmail.com>
2024-08-21 07:27:33 +0900

    [update] 문서 데이터 관련 파일 삭제 및 민감 데이터 탐지 버튼 연결

--- main.py
+++ main.py
@@ -1,35 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
 from PyQt5.QtGui import QPixmap
 from doc_del_ui import DocDelWindow  # DocDelWindow로 변경
-from sen_data_main import SenDataMainWindow  # sen_data_main에서 SenDataMainWindow 직접 가져오기
-
-
-class BrowseWindow(QWidget):
-    def __init__(self):
-        super(BrowseWindow, self).__init__()
-
-        self.setWindowTitle("Browse File")
-        self.setFixedSize(400, 300)
-
-        layout = QVBoxLayout()
-        self.label = QLabel("This is the Browse File window", self)
-        layout.addWidget(self.label)
-
-        self.setLayout(layout)
-
-class DeleteWindow(QWidget):
-    def __init__(self):
-        super(DeleteWindow, self).__init__()
-
-        self.setWindowTitle("Delete File")
-        self.setFixedSize(400, 300)
-
-        layout = QVBoxLayout()
-        self.label = QLabel("This is the Delete File window", self)
-        layout.addWidget(self.label)
-
-        self.setLayout(layout)
+from img_detect import ImageScanScreen  # img_detect에서 ImageScanScreen 가져오기
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -59,7 +32,7 @@ class MainWindow(QMainWindow):
         self.browse_button.move(browse_button_x, browse_button_y)
         self.browse_button.clicked.connect(self.open_doc_del_ui_window)
 
-        # Delete button setup
+        # Delete button setup (민감 데이터 탐지)
         delete_button_x = 440
         delete_button_y = 530
         delete_button_width = 360
@@ -74,21 +47,17 @@ class MainWindow(QMainWindow):
             }}
         """)
         self.delete_button.move(delete_button_x, delete_button_y)
-        self.delete_button.clicked.connect(self.open_sen_data_main_window)
-
-    def open_browse_window(self):
-        self.browse_window = BrowseWindow()
-        self.browse_window.show()
+        self.delete_button.clicked.connect(self.open_img_detect_window)
 
     def open_doc_del_ui_window(self):
         # doc_del_ui.py의 DocDelWindow 클래스를 사용하여 새 창 열기
         self.doc_del_ui_window = DocDelWindow()
         self.doc_del_ui_window.show()
 
-    def open_sen_data_main_window(self):
-        # sen_data_main.py의 SenDataMainWindow 클래스를 사용하여 새 창 열기
-        self.sen_data_main_window = SenDataMainWindow()
-        self.sen_data_main_window.show()
+    def open_img_detect_window(self):
+        # img_detect.py의 ImageScanScreen 클래스를 사용하여 새 창 열기
+        self.img_detect_window = ImageScanScreen()
+        self.img_detect_window.show()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
